37923e543a1acf8099a5877cb8bb6a33a15c6571
Akshay <komaraakshay1@gmail.com>
2026-06-24 11:52:51 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const notes = [];
+
+//Post - create a note 
+app.post("/notes",function(req,res){
+    const note = req.body.note;
+    notes.push(note);
+
+    res.json({
+        message : "Done"
+    })
+})
+
+
+//Get - get all my notes
+app.get("/notes",function(req,res){
+    res.json({
+        notes
+    })
+})
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.listen(3000);
